6eb6edaa2ce64e0a61eb293258f2e5dfdb91c391
caigua <caigua@huanleguang.com>
2017-11-09 19:45:07 +0800

    FillStyle

--- app/config/routes.js
+++ app/config/routes.js
@@ -11,6 +11,7 @@ import Triangle001 from '../pages/basic-shapes/triangle/001';
 import Rectangle001 from '../pages/basic-shapes/rectangle/001';
 import Circle001 from '../pages/basic-shapes/circle/001';
 import Stroke from '../pages/basic-shapes/stroke/001';
+import Fill from '../pages/basic-shapes/fill/001';
 
 import DrawImage from '../pages/draw-image/index';
 import Halloween from '../pages/draw-image/instances/halloween';
@@ -25,6 +26,7 @@ export default [
     { path: '/basic-shapes/rectangle/001', component: Rectangle001 },
     { path: '/basic-shapes/circle/001', component: Circle001 },
     { path: '/basic-shapes/stroke/001', component: Stroke },
+    { path: '/basic-shapes/fill/001', component: Fill },
 
     { path: '/draw-image', component: DrawImage },
     { path: '/draw-image/instances/halloween', component: Halloween },
---
+++ app/pages/basic-shapes/fill/001.vue
@@ -0,0 +1,32 @@
+<template>
+    <main>
+        <canvas id="canvas" width="800" height="600"></canvas>
+    </main>
+</template>
+
+<script>
+    export default {
+        methods: {
+            draw() {
+                let canvas = document.getElementById('canvas');
+                let context = canvas.getContext('2d');
+
+                context.moveTo(100, 100);
+                context.lineTo(300, 300);
+                context.lineTo(300, 100);
+                context.lineTo(100, 100);
+
+                // 定义线宽度
+                context.lineWidth = 10;
+
+                // 定义笔触颜色
+                context.fillStyle = '#c7254e';
+                // 绘制
+                context.fill();
+            }
+        },
+        mounted() {
+            this.draw();
+        }
+    }
+</script>
