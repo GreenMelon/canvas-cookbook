cbc80aa2b0d4c2f2226f79ff57eb026e579eec4c
caigua <caigua@huanleguang.com>
2018-07-19 21:08:49 +0800

    Image - Merge Image

--- app/pages/image/merge/01.vue
+++ app/pages/image/merge/01.vue
@@ -1,4 +1,4 @@
-<style>
+<style scoped>
     .container {
         height: 100px;
         overflow: hidden;
@@ -7,20 +7,41 @@
         z-index: -1;
         opacity: 0;
     }
+    img {
+        width: 300px;
+        height: auto;
+    }
 </style>
 
 <template>
     <main>
         <div class="container">
-            <canvas id="canvas" height="3000"></canvas>
+            <canvas id="canvas" height="3000" width="790"></canvas>
         </div>
-        
-        <img :src="exportedImageUrl">
-        <button @click="exportImage" type="button">export</button>
+
+        <ul>
+            <li
+                v-for="(src, index) in exportedImages"
+                :key="index"
+            >
+                <img :src="src" alt="">
+            </li>
+        </ul>
     </main>
 </template>
 
 <script>
+    /**
+     * Step
+     * url => { width, height, url }
+     * remain index
+     * calc height (best pratice)
+     * 
+     * drawImage
+     * exportImage
+     * saveImage
+     */
+    import Promise from 'bluebird';
     export default {
         data() {
             return {
@@ -41,39 +62,89 @@
                         top: 1554,
                     }
                 ],
-                exportedImageUrl: '',
+                exportedImages: [],
+                maxHeight: 1546,
             }
         },
         methods: {
-            init() {
+            initCanvas() {
                 this.canvas = document.getElementById('canvas');
                 this.context = this.canvas.getContext('2d');
             },
-            drawImages(images) {
-                const { canvas, context } = this;
-                images = images.map(image => {
+            loadImages(images) {
+                const newImages = images.map(image => {
                     return Object.assign(image, {
                         url: `http://st-cjxq.dancf.com/20180717/makeshot_${image.shot}.jpg?x-oss-process=image/resize,w_790/sharpen,95`,
                     });
                 });
 
-                images.forEach(image => {
-                    const img = new Image();
-                    img.setAttribute('crossOrigin', 'anonymous');
-                    img.src = image.url;
-                    img.onload = () => {
-                        canvas.width = img.width;
-                        context.drawImage(img, 0, image.top);
-                    }
+                const loadEvents = newImages.map(image => {
+                    return new Promise((resolve) => {
+                        const img = new Image();
+                        img.setAttribute('crossOrigin', 'anonymous');
+                        img.src = image.url;
+                        console.log(img.src);
+                        img.onload = () => {
+                            resolve(img);
+                        }
+                    });
+                });
+
+                Promise.all(loadEvents).then(res => {
+                    this.drawImages(res);
                 });
             },
-            exportImage() {
-                this.exportedImageUrl = this.canvas.toDataURL('image/png');
+            drawImages(images) {
+                const { canvas, context } = this;
+                const groups = this.groupImages(images, this.maxHeight);
+
+                groups.forEach(group => {
+                    canvas.height = group.height;
+                    canvas.width = group.images[0].width;
+                    context.clearRect(0, 0, canvas.width, canvas.height);
+
+                    let top = 0;
+                    group.images.forEach(image => {
+                        context.drawImage(image, 0, top);
+                        top += image.height;
+                    });
+                    this.exportImage();
+                });
+
+                console.log(this.exportedImages);
             },
+            groupImages(images, limit) {
+                return images.reduce((groups, image) => {
+                    if (!groups.length) {
+                        groups.push({
+                            images: [],
+                            height: 0,
+                        });
+                    }
+                    const group = groups[groups.length - 1]
+
+                    if (group.height + image.height > limit) {
+                        groups.push({
+                            images: [image],
+                            height: image.height,
+                        });
+                    } else {
+                        group.images.push(image);
+                        group.height += image.height;
+                    }
+
+                    return groups;
+                }, []);
+            },
+            exportImage() {
+                let url = canvas.toDataURL('image/png');
+                this.exportedImages.push(url);
+            },
+            saveImages() {},
         },
         mounted() {
-            this.init();
-            this.drawImages(this.images);
+            this.initCanvas();
+            this.loadImages(this.images);
         },
     };
 </script>
