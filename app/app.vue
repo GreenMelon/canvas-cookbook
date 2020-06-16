a9f5c94a088f98226cd4256a5072112d98fb930c
caigua <caigua@huanleguang.com>
2020-06-16 14:00:09 +0800

    :art: 调整代码结构

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,3 @@
-<style lang="less">
-    @import './less/main.less';
-</style>
-
 <template>
     <div id="app">
         <modules :modules="modules"></modules>
@@ -10,28 +6,32 @@
 </template>
 
 <script>
-    import './components/modules';
-    import './components/categories';
+import './components/modules';
+import './components/categories';
 
-    export default {
-        data() {
-            return {
-                modules: [
-                    { route: '/path-and-status', name: '状态和路径' },
-                    { route: '/shapes', name: '基本形状' },
-                    { route: '/text', name: '文本' },
-                    { route: '/image', name: '图像' },
-                    { route: '/pixel', name: '像素' },
-                    { route: '/gradient', name: '渐变' },
-                    { route: '/shadow', name: '阴影' },
-                    { route: '/pattern', name: '图章' },
-                    { route: '/transform', name: '变形' },
-                    { route: '/animation', name: '动画' },
-                    { route: '/events', name: '事件' },
-                    { route: '/instances', name: '实例' },
-                    { route: '/plugins', name: '插件' },
-                ],
-            }
-        }
-    };
+export default {
+    data() {
+        return {
+            modules: [
+                { route: '/path-and-status', name: '状态和路径' },
+                { route: '/shapes', name: '基本形状' },
+                { route: '/text', name: '文本' },
+                { route: '/image', name: '图像' },
+                { route: '/pixel', name: '像素' },
+                { route: '/gradient', name: '渐变' },
+                { route: '/shadow', name: '阴影' },
+                { route: '/pattern', name: '图章' },
+                { route: '/transform', name: '变形' },
+                { route: '/animation', name: '动画' },
+                { route: '/events', name: '事件' },
+                { route: '/instances', name: '实例' },
+                { route: '/plugins', name: '插件' },
+            ],
+        };
+    },
+};
 </script>
+
+<style lang="less">
+@import './less/main.less';
+</style>
--- app/components/modules.vue
+++ app/components/modules.vue
@@ -12,24 +12,18 @@
 </template>
 
 <script>
-    import Vue from 'vue';
+import Vue from 'vue';
 
-    const Modules = Vue.extend({
-        props: {
-            modules: {
-                type: Array,
-                default: [],
-            },
+const Modules = Vue.extend({
+    props: {
+        modules: {
+            type: Array,
+            default: [],
         },
-        data() {
-            return {}
-        },
-        methods: {},
-        mounted() {},
-    });
+    },
+});
 
-    Vue.component('modules', Modules);
+Vue.component('modules', Modules);
 
-    export default Modules;
-
+export default Modules;
 </script>
