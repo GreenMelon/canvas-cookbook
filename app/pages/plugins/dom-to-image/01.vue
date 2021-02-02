837db1b78eb1f7bafa8e28081b3f67be7817bfca
caigua <caigua@huanleguang.com>
2021-02-02 09:51:43 +0800

    :sparkles: dom-to-image

--- app/pages/plugins/dom-to-image/01.vue
+++ app/pages/plugins/dom-to-image/01.vue
@@ -1,16 +1,16 @@
 <template>
     <main>
         <div id="editor">
-            <h1>caigua 菜瓜</h1>
+            <h1>文字</h1>
             <img
                 src="https://st-gdx.dancf.com/gaodingx/368/design/20190715-104036-8e38.png"
                 :style="{
                     transform: `scale(${zoom})`,
                     transformOrigin: '0 0',
                 }"
-                alt=""
             >
         </div>
+
         <div id="shot"></div>
     </main>
 </template>
@@ -34,10 +34,23 @@ export default {
             const editor = document.getElementById('editor');
             const shot = document.getElementById('shot');
 
-            DomToImage.toPng(editor)
+            const { offsetWidth, offsetHeight } = editor;
+            const scale = 2;
+            const option = {
+                width: offsetWidth * scale,
+                height: offsetHeight * scale,
+                style: {
+                    // transform: `scale(${scale}) translate(${offsetWidth / 2 / scale}px, ${offsetHeight / 2 / scale}px)`,
+                    transform: `scale(${scale})`,
+                    transformOrigin: '0 0',
+                },
+            };
+
+            DomToImage.toPng(editor, option)
                 .then((dataUrl) => {
                     const img = new Image();
                     img.src = dataUrl;
+                    img.width = offsetWidth;
                     shot.appendChild(img);
                 })
                 .catch((error) => {
